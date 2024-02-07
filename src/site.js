0d807be9b9f23bf9dd240fc5318b4343413cc76e
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2024-02-07 17:06:53 +1000

    Worked on animations
    Worked on animations / updates npm packages

--- src/site.js
+++ src/site.js
@@ -1,15 +1,7 @@
 // letters to bounce in from top of screen when page loads
+gsap.from(".letter", {duration: 4, y: -200, stagger: .5, ease: "bounce"})
 
-gsap.from("#t", {duration: 4, y: -300, opacity: 0, delay: 0, ease: "bounce"})
-gsap.from("#a", {duration: 4, y: -300, opacity: 0, delay: 0.5, ease: "bounce"})
-gsap.from("#y", {duration: 4, y: -300, opacity: 0, delay: 1, ease: "bounce"})
-gsap.from("#l", {duration: 4, y: -300, opacity: 0, delay: 1.5, ease: "bounce"})
-gsap.from("#aa", {duration: 4, y: -300, opacity: 0, delay: 2, ease: "bounce"})
-gsap.from("#dot", {duration: 4, y: -300, opacity: 0, delay: 2.5, ease: "bounce"})
-gsap.from("#i", {duration: 4, y: -300, opacity: 0, delay: 3, ease: "bounce"})
-gsap.from("#o", {duration: 4, y: -300, opacity: 0, delay: 3.5, ease: "bounce"})
-
-// letters to rotate once positioned
+// letters to rotate and change respective colour once positioned
 gsap.from("#t", {rotation: '+=360', duration: 1, color: "purple", delay: 4.5})
 gsap.from("#a", {rotation: '+=360', duration: 1, color: "pink", delay: 5})
 gsap.from("#y", {rotation: '+=360', duration: 1, color: "green", delay: 5.5})
@@ -18,12 +10,6 @@ gsap.from("#aa", {rotation: '+=360', duration: 1, color: "blue", delay: 6.5})
 gsap.from("#dot", {rotation: '+=360', duration: 1, color: "green", delay: 7})
 gsap.from("#i", {rotation: '+=360', duration: 1, color: "yellow", delay: 7.5})
 gsap.from("#o", {rotation: '+=360', duration: 1, color: "orange", delay: 8})
-
-// animate ripple effect when chechbox is checked and unchecked
-
-const ripple = () => {
-    gsap.fromTo('.circle', 5,{scale: 0, autoAlpha:1, transformOrigin: "center center"}, {scale: 5, autoAlpha: 0, repeat: 0});
-}
 
 // animate checkbox and photo to rotate
 const startAnimate = gsap.timeline({ease: Linear.easeNone, repeat: -1})
@@ -34,16 +20,14 @@ checkbox.addEventListener('click', (e) =>{
     
     if (checkbox.checked === true) {
         startAnimate.resume()
-        ripple()
 
     } else if (checkbox.checked === false) {
         startAnimate.pause()
-        ripple()
     }
 
 })
 
-//create animation timeline and settings
+//create monster animation timeline and settings
 const animate = gsap.timeline({repeat:-1}).timeScale(.5)
 animate.to(".monsters", {y: +90, rotation: '+=360', stagger: 0.25, ease:'elastic'}).pause()
 
@@ -56,8 +40,31 @@ document.querySelector('#pause').addEventListener('click', (e) => {
     animate.pause()
  })
 
+//create tile effect to animate
+gsap.to(".tiles", {
+    duration: 3,
+    repeat: -1,
+    scale: 0,
+    y: 40,
+    ease: "back",
+    stagger: {
+      grid: "auto",
+      from: "start",
+      amount: 1.5
+    }
+  });
 
-
+  gsap.to(".box", {
+    y: 100,
+    stagger: {
+      // wrap advanced options in an object
+      each: 0.1,
+      from: "center",
+      grid: "auto",
+      ease: "power2.inOut",
+      repeat: -1, // Repeats immediately, not waiting for the other staggered animations to finish
+    },
+  });
 
 
 
