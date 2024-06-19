c12ae52af08528971292c46ff95b4a5537a24b64
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2024-06-19 10:27:37 +1000

    Updated
    Added image slider to site

--- src/site.js
+++ src/site.js
@@ -11,20 +11,124 @@ gsap.from("#dot", {rotation: '+=360', duration: 1, color: "green", delay: 7})
 gsap.from("#i", {rotation: '+=360', duration: 1, color: "yellow", delay: 7.5})
 gsap.from("#o", {rotation: '+=360', duration: 1, color: "orange", delay: 8})
 
-// animate checkbox and photo to rotate
-const startAnimate = gsap.timeline({ease: Linear.easeNone, repeat: -1})
-startAnimate.to("#animation", {rotation: '+=360'}).pause()
-const checkbox = document.querySelector("#checkbox")
+//button to slide menu open and closed
+const menu = document.querySelector("#menu-button")
+const toggleMenu = document.querySelector(".menu-slide")
+menu.addEventListener("click", function (e) {
+    e.preventDefault()
+    toggleMenu.classList.toggle("menu-slide-active")
 
-checkbox.addEventListener('click', (e) =>{
-    
-    if (checkbox.checked === true) {
-        startAnimate.resume()
+})
 
-    } else if (checkbox.checked === false) {
-        startAnimate.pause()
+//button from menu to logout
+const logout = document.querySelector("#optionFour")
+logout.addEventListener("click", function(e) {
+    e.preventDefault()
+    location.assign("/index.html")
+})
+
+//image slider code
+
+const selectBox = document.querySelector(".slide-box")
+const guageOne = document.querySelector(("#guageOne"))
+const guageTwo = document.querySelector(("#guageTwo"))
+const guageThree = document.querySelector(("#guageThree"))
+const guageFour = document.querySelector(("#guageFour"))
+const guageFive = document.querySelector(("#guageFive"))
+
+
+selectBox.style.left = 0
+
+const transitionRight = function () {
+    var num = parseInt(selectBox.style.left)
+    if (num === -1200) {
+        return
+    } else {
+        selectBox.style.left = num - 300 + "px"
+        selectBox.style.transition = "all 3s"
     }
 
+    if (num === -900) {
+        guageFive.style.backgroundColor = "white"
+        guageFive.style.transition = "all 3s"
+    } else {
+        guageFive.style.backgroundColor = "transparent"
+    }
+
+    if (num === -600) {
+        guageFour.style.backgroundColor = "white"
+        guageFour.style.transition = "all 3s"
+    } else {
+        guageFour.style.backgroundColor = "transparent"
+    }
+
+    if (num === -300) {
+        guageThree.style.backgroundColor = "white"
+        guageThree.style.transition = "all 3s"
+    } else {
+        guageThree.style.backgroundColor = "transparent"
+    }
+
+    if (num === 0) {
+        guageTwo.style.backgroundColor = "white"
+        guageTwo.style.transition = "all 3s"
+        guageOne.style.backgroundColor = "transparent"
+        guageOne.style.transition = "all 3s"
+    } else {
+        guageTwo.style.backgroundColor = "transparent"
+    }
+    
+}
+
+const transitionLeft = function () {
+    var num = parseInt(selectBox.style.left)
+    if (num === 0) {
+        return
+    } else {
+        selectBox.style.left = num + 300 + "px"
+        selectBox.style.transition = "all 3s"
+    }
+
+    if (num === -1200) {
+        guageFive.style.backgroundColor = "transparent"
+        guageFive.style.transition = "all 3s"
+        guageFour.style.backgroundColor = "white"
+        guageFour.style.transition = "all 3s"
+    } 
+
+    if (num === -900) {
+        guageFour.style.backgroundColor = "transparent"
+        guageFour.style.transition = "all 3s"
+        guageThree.style.backgroundColor = "white"
+        guageThree.style.transition = "all 3s"
+    } 
+
+    if (num === -600) {
+        guageThree.style.backgroundColor = "transparent"
+        guageThree.style.transition = "all 3s"
+        guageTwo.style.backgroundColor = "white"
+        guageTwo.style.transition = "all 3s"
+    } 
+
+    if (num === -300) {
+        guageTwo.style.backgroundColor = "transparent"
+        guageTwo.style.transition = "all 3s"
+        guageOne.style.backgroundColor = "white"
+        guageOne.style.transition = "all 3s"
+    } 
+
+}
+
+document.querySelector("#action-right").addEventListener("click", function (e) {
+    e.preventDefault()
+    transitionRight()
+    
+})
+
+document.querySelector("#action-left").addEventListener("click", function (e) {
+    e.preventDefault()
+    transitionLeft()
+    
 })
 
 //create monster animation timeline and settings
@@ -40,19 +144,7 @@ document.querySelector('#pause').addEventListener('click', (e) => {
     animate.pause()
  })
 
-//create tile effect to animate
-gsap.to(".tiles", {
-    duration: 3,
-    repeat: -1,
-    scale: 0,
-    // y: 40,
-    ease: "back",
-    stagger: {
-      grid: "auto",
-      from: "start",
-      amount: 1.5
-    }
-  });
+
 
 
 
